f1ea1c431c0fb0542135cdc819434d4f1d534194
drazerrr <anuragdwivedi9794@gmail.com>
2024-03-09 14:51:41 +0530

    feat: add CRUD operations for video comments
    This commit adds the ability to:
    
    - Create a new comment on a video
    - Update an existing comment on a video
    - Delete a comment on a video
    
    Changes:
    
    - Added createComment controller method
    - Added updateComment controller method
    - Added deleteComment controller method
    - Added comment routes for CRUD operations
    - Added comment validation middleware

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose"
 import {Comment} from "../models/comment.model.js"
+import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -13,14 +14,84 @@ const getVideoComments = asyncHandler(async (req, res) => {
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {videoId} = req.params
+    const {comment} = req.body
+    if(comment?.trim() === "") {
+        throw new ApiError(400, "Comment text is required")
+    }
+    const user = await User.findById(req.user?._id);
+    if(!user) {
+        throw new ApiError(404, "User not found")
+    }
+    const newComment = await Comment.create({
+        comment,
+        video: new mongoose.Types.ObjectId(videoId),
+        owner: user._id
+    })
+
+    if(!newComment) {
+        throw new ApiError(400, "Something went wrong while creating comment")
+    }
+
+    return res
+    .status(201)
+    .json(
+        new ApiResponse(
+            200,
+            newComment,
+            "Comment created Successfully"
+        )
+    )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
     // TODO: update a comment
+    const {commentId} = req.params
+    const {comment} = req.body
+    if(comment?.trim() === "") {
+        throw new ApiError(400, "Comment text is required")
+    }
+
+    const commentToUpdate = await Comment.findById(commentId)
+    if(!commentToUpdate) {
+        throw new ApiError(404, "Comment not found")
+    }
+    if(req.user?._id.toString()!== commentToUpdate.owner.toString()) {
+        throw new ApiError(401, "You are not authorized to update this comment")
+    }
+    commentToUpdate.comment = comment
+    const updatedComment = await commentToUpdate.save()
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            updatedComment,
+            "Comment updated Successfully"
+        )
+    )
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params
+    const commentToDelete = await Comment.findById(commentId)
+    if(!commentToDelete) {
+        throw new ApiError(404, "Comment not found")
+    }
+    if(req.user?._id.toString()!== commentToDelete.owner.toString()) {
+        throw new ApiError(401, "You are not authorized to delete this comment")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "Comment deleted Successfully"
+        )
+    )
 })
 
 export {
